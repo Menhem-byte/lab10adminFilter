f085eb924afd2b62b17ba03f59433815714be2ba
789438 <789438@SAIT225754IT.ACDM.DS.SAIT.CA>
2020-11-14 23:31:23 -0700

    lab 10 implementing admin filter

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -18,6 +18,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.User;
+import services.AccountService;
 
 /**
  *
@@ -35,7 +37,10 @@ public class AdminFilter implements Filter {
       HttpServletRequest httpRequest=(HttpServletRequest) request;
       HttpSession session=httpRequest.getSession();
       String email= (String) session.getAttribute("email");
-      if(!email.equals("cprg352+admin@gmail.com")){
+      
+      AccountService account =new AccountService();
+      User user = account.get(email);
+      if(user.getRole().getRoleId() != 1){
      HttpServletResponse httpResponse =(HttpServletResponse) response;
       httpResponse.sendRedirect("notes");
       return;
